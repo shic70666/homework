eebb714ece8a6a4bb93bfee6d2b55927f339811f
Chen Shi <shic70666@outlook.com>
2022-04-12 19:36:18 +0200

    CS: [change output data format as .json]

--- SHM/Multiprocess_data.py
+++ SHM/Multiprocess_data.py
@@ -86,28 +86,25 @@ def run_ChenShi(data_path):
     ns = 128 # length of frames
     nss = 16 # width of feature matrix
 
-    each_case = pd.DataFrame() 
-    for ch_num in range(0, len(data_case)): 
-        name1 = 'C_{}_CH_{}_frames'.format(case_num, ch_num+1) #格式化字符串
-        name2 = 'C_{}_CH_{}_feature_matrixs'.format(case_num, ch_num+1)
-        name3 = 'C_{}_CH_{}_labels'.format(case_num, ch_num+1)
-        label = 'C_{}_CH_{}'.format(case_num, ch_num+1)
-        each_channel = pd.DataFrame( columns=[name1, name2, name3] ) 
-        
+    each_case = pd.DataFrame()
+    for ch_num in range(0, len(data_case)):
+        name = 'CH{}'.format(ch_num+1)
+        each_channel = pd.DataFrame( columns=[name] )
+
         frames, feature_matrixs = [], []
         for i in range(0, len(data_case[ch_num])//ns):
             frame = data_case[ch_num][i*ns: (i+1)*ns]
             frames.append(frame)
-            
+
             frame_temp = frame.reshape((len(frame)//nss, nss))
             feature_matrixs.append(frame_temp)
-            each_channel = each_channel.append([{name1:frame, name2:frame_temp,
-                                                name3:label}], ignore_index=True) 
-        
-        each_case = pd.concat([each_case, each_channel], axis=1) 
-        
+            each_channel = each_channel.append([{name:frame_temp,
+                                                }], ignore_index=True)
+
+        each_case = pd.concat([each_case, each_channel], axis=1)
+
         filename = dataFile.stem
-        each_case.to_csv(filename+".csv")
+        each_case.to_json(filename+".json")
 
 
 def run_multiprocess(list_data_path,):
--- SHM/Singleprocess_data.py
+++ SHM/Singleprocess_data.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import scipy.signal as signal
 from pathlib import Path
 
-dataFile = Path('~/Codes/homework/SHM/shm01s.mat').expanduser()
+dataFile = Path('~/SHM_Data/Shaker/Random/shm01s.mat').expanduser()
 X = scio.loadmat(dataFile)
 data_all = X['dasy']
 
@@ -29,11 +29,8 @@ nss = 16 # width of feature matrix
 
 each_case = pd.DataFrame()
 for ch_num in range(0, len(data_case)):
-    name1 = 'C_{}_CH_{}_frames'.format(case_num, ch_num+1) #格式化字符串
-    name2 = 'C_{}_CH_{}_feature_matrixs'.format(case_num, ch_num+1)
-    name3 = 'C_{}_CH_{}_labels'.format(case_num, ch_num+1)
-    label = 'C_{}_CH_{}'.format(case_num, ch_num+1)
-    each_channel = pd.DataFrame( columns=[name1, name2, name3] )
+    name = 'CH{}'.format(ch_num+1)
+    each_channel = pd.DataFrame( columns=[name] )
 
     frames, feature_matrixs = [], []
     for i in range(0, len(data_case[ch_num])//ns):
@@ -42,18 +39,46 @@ for ch_num in range(0, len(data_case)):
 
         frame_temp = frame.reshape((len(frame)//nss, nss))
         feature_matrixs.append(frame_temp)
-        each_channel = each_channel.append([{name1:frame, name2:frame_temp,
-                                             name3:label}], ignore_index=True)
+        each_channel = each_channel.append([{name:frame_temp,
+                                             }], ignore_index=True)
 
     each_case = pd.concat([each_case, each_channel], axis=1)
 
     filename = dataFile.stem
-    # each_case.to_csv(filename+".csv")
     each_case.to_json(filename+".json")
 
+# Version 1: generate .csv files
+# case_num = 1
+
+# ns = 128 # length of frames
+# nss = 16 # width of feature matrix
+
+# each_case = pd.DataFrame()
+# for ch_num in range(0, len(data_case)):
+#     name1 = 'C_{}_CH_{}_frames'.format(case_num, ch_num+1) #格式化字符串
+#     name2 = 'C_{}_CH_{}_feature_matrixs'.format(case_num, ch_num+1)
+#     name3 = 'C_{}_CH_{}_labels'.format(case_num, ch_num+1)
+#     label = 'C_{}_CH_{}'.format(case_num, ch_num+1)
+#     each_channel = pd.DataFrame( columns=[name1, name2, name3] )
+
+#     frames, feature_matrixs = [], []
+#     for i in range(0, len(data_case[ch_num])//ns):
+#         frame = data_case[ch_num][i*ns: (i+1)*ns]
+#         frames.append(frame)
+
+#         frame_temp = frame.reshape((len(frame)//nss, nss))
+#         feature_matrixs.append(frame_temp)
+#         each_channel = each_channel.append([{name1:frame, name2:frame_temp,
+#                                              name3:label}], ignore_index=True)
+
+#     each_case = pd.concat([each_case, each_channel], axis=1)
+
+#     filename = dataFile.stem
+#     each_case.to_csv(filename+".csv")
 
 
 
 
 
 
+# %%
